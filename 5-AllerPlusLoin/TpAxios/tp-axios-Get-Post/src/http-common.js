3d0fc657101251e503b4c939a9b26884b3f8dcc8
Anthony <github@utopios.net>
2021-06-25 16:16:57 +0200

    Remplacement fichiers

--- 5-AllerPlusLoin/TpAxios/tp-axios-Get-Post/src/http-common.js
+++
@@ -1,8 +0,0 @@
-//import axios from 'axios';
-const axios = require('axios').default;
-export const HTTP = axios.create({
-  baseURL: `http://jsonplaceholder.typicode.com/`,
-  headers: {
-    Authorization: 'Bearer {token}'
-  }
-})
